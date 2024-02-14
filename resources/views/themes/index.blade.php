10bd380248724d25a8c26e59cdc77aa5ab559da3
Fady Mondy <info@3x1.io>
2024-02-14 16:01:25 +0200

    fix theme security access to file by config

--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -7,10 +7,11 @@
             <x-splade-form class="mx-2" action="{{route('admin.themes.index')}}" method="GET">
                 <x-splade-input @change="form.submit()" placeholder="Search By Theme Name" name="search" type="search"></x-splade-input>
             </x-splade-form>
-
-            <Link modal href="{{route('admin.themes.upload')}}" class="filament-button inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 filament-page-button-action">
-                {{__('Upload Theme')}}
-            </Link>
+            @if(config('tomato-themes.allow_upload'))
+                <Link modal href="{{route('admin.themes.upload')}}" class="filament-button inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 filament-page-button-action">
+                    {{__('Upload Theme')}}
+                </Link>
+            @endif
         </div>
     </x-slot:buttons>
 
@@ -32,9 +33,11 @@
                                 <a href="{{url('/')}}" target="_blank" class=" mx-2 filament-button inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 filament-page-button-action">
                                     {{__('Preview')}}
                                 </a>
+                                @if(config('tomato-themes.allow_create'))
                                 <Link href="{{route('admin.themes.custom', $theme['name'])}}" class="filament-button inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 filament-page-button-action">
                                     {{__('Customize')}}
                                 </Link>
+                                @endif
                             </div>
                         </div>
                         @else
@@ -51,22 +54,25 @@
                                         }" class="filament-button inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 filament-page-button-action">
                                         {{__('Activate')}}
                                     </x-splade-link>
+                                    @if(config('tomato-themes.allow_destory'))
                                     <x-splade-link confirm href="{{route('admin.themes.destroy',$theme['name'])}}" method="DELETE" class="filament-button inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-red-600 hover:bg-red-500 focus:bg-red-700 focus:ring-offset-red-700 filament-page-button-action">
                                         {{__('Delete')}}
                                     </x-splade-link>
+                                    @endif
                                 </div>
                             </div>
                         @endif
                     </div>
                 @endforeach
 
-
+                @if(config('tomato-themes.allow_create'))
                 <Link href="{{route('admin.themes.create')}}" class="h-[300px] border border-dashed hover:border-solid hover:border-primary-500 border-4 border-gray-200 hover:border-none hover:bg-primary-500 text-gray-300 hover:text-white">
                     <div class="flex flex-col justify-center h-full items-center">
                         <x-heroicon-s-plus-circle class="w-32 h-32"/>
                         <h1>{{__('Add New Theme')}}</h1>
                     </div>
                 </Link>
+                @endif
             </div>
         </x-splade-data>
     </div>
